3dd914ce8412bef9b73ff5ba2c7e97eb3b3bee17
SANKOHOLDING\mine.topcuoglu <mine.topcuoglu@sanko.com.tr>
2023-07-05 09:50:16 +0300

    05.07 - Create Layers

---
+++ IdentityProject.PresentationLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityProject.PresentationLayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
