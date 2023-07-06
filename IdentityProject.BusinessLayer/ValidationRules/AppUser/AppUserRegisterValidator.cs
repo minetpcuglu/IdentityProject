70c65e633d44a28e4fb78d76488d0c7dfb9c9d9c
SANKOHOLDING\mine.topcuoglu <mine.topcuoglu@sanko.com.tr>
2023-07-06 10:19:21 +0300

    06.07 / Fluent Validation

---
+++ IdentityProject.BusinessLayer/ValidationRules/AppUser/AppUserRegisterValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using IdentityProject.DTOLayer.DTOs.AppUserDto;
+
+namespace IdentityProject.BusinessLayer.ValidationRules.AppUser
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("İsim boş geçilemez");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyadı boş geçilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().Equal(y=>y.Password).WithMessage("Şifre Tekrarı boş geçilemez veya şifreler eşleşmiyor");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre boş geçilemez");
+            RuleFor(x => x.Mail).EmailAddress().NotEmpty().WithMessage("Mail boş geçilemez veya Mail Formatında yazın");
+            RuleFor(x => x.Username).MaximumLength(3).MaximumLength(50).NotEmpty().WithMessage("Kullanıcı Adı 3-50 karakter arasında olmalıdır");
+        }
+
+    }
+}
